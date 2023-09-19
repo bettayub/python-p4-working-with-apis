44492efe293f7783c36851c6534d6c71b1bcc5d9
bettayub <bettayub19@gmail.com>
2023-09-19 12:24:16 +0300

    completed assignment

--- lib/nyc_api.py
+++ lib/nyc_api.py
@@ -1,15 +1,51 @@
 
+# import requests
+# import json
+
+# class GetPrograms:
+
+#   def get_programs(self):
+#     URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
+
+#     response = requests.get(URL)
+#     return response.content
+
+# def program_school(self):
+#     # we use the JSON library to parse the API response into nicely formatted JSON
+#     programs_list = []
+#     programs = json.loads(self.get_programs())
+#     for program in programs:
+#             programs_list.append(program["agency"])
+
+#     return programs_list
+  
+# programs = GetPrograms()
+# programs_schools = programs.program_school()
+
+# for school in set(programs_schools):
+#     print(school)
+
 import requests
 import json
 
 class GetPrograms:
+    def get_programs(self):
+        URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
+        response = requests.get(URL)
+        return response.content
 
-  def get_programs(self):
-    URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
+    def program_school(self):
+        # we use the JSON library to parse the API response into nicely formatted JSON
+        programs_list = []
+        programs = json.loads(self.get_programs())
+        for program in programs:
+            programs_list.append(program["agency"])
 
-    response = requests.get(URL)
-    return response.content
+        return programs_list
 
+programs = GetPrograms()
+programs_schools = programs.program_school()
 
-programs = GetPrograms().get_programs()
-print(programs)
+for school in set(programs_schools):
+    print(school)
+
